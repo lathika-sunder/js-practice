1ea04dae16735d7ca5f2965d9fc74ce2c19e3b92
Lathika Sunder <lathikasunder11@gmail.com>
2024-02-09 19:05:54 +0530

    Objects

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-console.log("Hi")
+console.log("Hi");
 
-var a=45
-var b=8
-console.log(a+b)
-
+var a = 45;
+var b = 8;
+console.log(a + b);
 
 //On click functon
 // document.getElementById("change-btn").onclick = function() {
@@ -14,87 +13,98 @@ console.log(a+b)
 // var color=prompt("What is your favourite colour")
 
 //Funtions and arguments with while loop and for loop
-function fun(num){
-    
-   
-    while(num<=100)
-    {
-        num++;
-    }
-    for(let i=0;i<100;i++)
-    {
-        num++;
-    }
-    
-
+function fun(num) {
+  while (num <= 100) {
+    num++;
+  }
+  for (let i = 0; i < 100; i++) {
+    num++;
+  }
 }
 
 //Datatypes - number, string
-{key:"value"}//object
-let nums=[89,90,88,76]//array
-let abc;//undefined
-let no= null;//null
-
+{
+  key: "value";
+} //object
+let nums = [89, 90, 88, 76]; //array
+let abc; //undefined
+let no = null; //null
 
 //newline
-var result="hey\nI am here"
+var result = "hey\nI am here";
 
 //array operations
-console.log(result)
-console.log(result.length)
-console.log(result.indexOf(89))
+console.log(result);
+console.log(result.length);
+console.log(result.indexOf(89));
 console.log(result.toUpperCase());
 console.log(result.toLowerCase());
 console.log(result.charAt(2));
-console.log(result[3])
-console.log(result.slice(3,4))
+console.log(result[3]);
+console.log(result.slice(3, 4));
 console.log(result.split("\n"));
 
 //array - 2ways to create
-let fruits=["banana","apple","mango","peach","melon"]
-console.log(fruits)
-fruits=new Array("banana","apple","mango","peach","melon")
-console.log(fruits)
+let fruits = ["banana", "apple", "mango", "peach", "melon"];
+console.log(fruits);
+fruits = new Array("banana", "apple", "mango", "peach", "melon");
+console.log(fruits);
 
 //array operations
-console.log(fruits[0])
-console.log(fruits.toString())
-console.log(fruits.join(" * "))
-console.log(fruits.pop(),fruits)//removes last element
-console.log(fruits.push("kiwi"),fruits)//add at the end
-fruits[fruits.length]="new fruit"
-console.log(fruits)
-let veg=['carrot','brinjal','onion']
-let allgroceries=fruits.concat(veg)
-console.log(allgroceries)
-console.log(allgroceries.reverse())
-console.log(allgroceries.slice(0,5))
-console.log(nums.sort(function(a,b){return a-b}))//sorted in ascending order
-console.log(nums.sort(function(a,b){return b-a}))//sorted in descending order
+console.log(fruits[0]);
+console.log(fruits.toString());
+console.log(fruits.join(" * "));
+console.log(fruits.pop(), fruits); //removes last element
+console.log(fruits.push("kiwi"), fruits); //add at the end
+fruits[fruits.length] = "new fruit";
+console.log(fruits);
+let veg = ["carrot", "brinjal", "onion"];
+let allgroceries = fruits.concat(veg);
+console.log(allgroceries);
+console.log(allgroceries.reverse());
+console.log(allgroceries.slice(0, 5));
+console.log(
+  nums.sort(function (a, b) {
+    return a - b;
+  })
+); //sorted in ascending order
+console.log(
+  nums.sort(function (a, b) {
+    return b - a;
+  })
+); //sorted in descending order
 
-let array=new Array()
-for(let num=0;num<10;num++)
-{
-    array.push(num)
+let array = new Array();
+for (let num = 0; num < 10; num++) {
+  array.push(num);
 }
-console.log(array)
-
+console.log(array);
 
 //object operations
-let student={firName:'Lathika','lastName':'Sunder',age:19,city:"Chennai"}
+let student = {
+  firstName: "Lathika",
+  lastName: "Sunder",
+  age: 19,
+  city: "Chennai",
+  studentInfo:function()
+  {
+    return this.firstName+'\n'+this.lastName
+  }
+};
+console.log(student.firstName); //access obj elements
+console.log(student.studentInfo())
 
-
-function  changeColour(){
-    var box=document.getElementById('box')
-    var color=getRandomColour();
-    box.style.backgroundColor=color;
+//color changer
+function changeColour() {
+  var box = document.getElementById("box");
+  var color = getRandomColour();
+  box.style.backgroundColor = color;
 }
-function getRandomColour()
-{
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function getRandomColour() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
