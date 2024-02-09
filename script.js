829306a3d6bb8c2674779f831a7e6d60d48403e8
Lathika Sunder <lathikasunder11@gmail.com>
2024-02-09 15:06:23 +0530

    Lesson -2

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hi")
+
+var a=45
+var b=8
+console.log(a+b)
+
+//Change value in html file
+document.getElementById("btn").innerHTML="Hey"
+
+//On click functon
+document.getElementById("change-btn").onclick = function() {
+    alert("You just clicked me")
+}
+
+//use prompt to get input
+var color=prompt("What is your favourite colour")
