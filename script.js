86909674ec66609e465f8fe77964b2d24f0fa4e5
Lathika Sunder <lathikasunder11@gmail.com>
2024-02-09 18:14:26 +0530

    Lesson 3 and 5

--- script.js
+++ script.js
@@ -8,9 +8,26 @@ console.log(a+b)
 document.getElementById("btn").innerHTML="Hey"
 
 //On click functon
-document.getElementById("change-btn").onclick = function() {
-    alert("You just clicked me")
+// document.getElementById("change-btn").onclick = function() {
+//     alert("You just clicked me")
+// }
+
+// //use prompt to get input
+// var color=prompt("What is your favourite colour")
+
+//Funtions and arguments with while loop and for loop
+function fun(num){
+    
+   
+    while(num<=100)
+    {
+        num++;
+    }
+    for(let i=0;i<100;i++)
+    {
+        num++;
+    }
+    document.getElementById("demo").innerHTML = num;
+
 }
 
-//use prompt to get input
-var color=prompt("What is your favourite colour")
